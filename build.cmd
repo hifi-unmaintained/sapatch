d94477061b04e412a0c58d0bd82970c7a1d0c558
Toni Spets <toni.spets@iki.fi>
2013-10-25 10:24:02 +0300

    Disable selecting elf, new linker imported

--- build.cmd
+++ build.cmd
@@ -1,2 +1,3 @@
 copy client_noenc.dat client_noenc.exe
 tools\linker.exe src\status_gump.asm src\status_gump.inc client_noenc.exe tools\nasm.exe
+tools\linker.exe src\no_elf.asm src\no_elf.inc client_noenc.exe tools\nasm.exe
